eb3f2f594989b0238c3d1d2547ebec91f77cdac0
SergiiMal <ser4ester@gmail.com>
2023-01-27 22:30:19 +0200

    +1+2_dz

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+ const galleryElem = document.querySelector('.gallery');
+ const galleryInsertHtml = createNewGallery(galleryItems);
+
+ function createNewGallery(galleryItems){
+	
+	 return galleryItems
+		 .map(({ preview, original, description }) => {
+			 return `
+		<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+		 }).join('');
+	 
+ }
+galleryElem.innerHTML = galleryInsertHtml;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
