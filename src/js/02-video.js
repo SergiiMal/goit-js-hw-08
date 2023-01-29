56ddccb5cc729dff348bd06f9f86a582fe7eebab
SergiiMal <ser4ester@gmail.com>
2023-01-29 17:27:17 +0200

    dz/8-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,13 @@
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
- player.on('timeupdate', throttle(onPlay, 1000));
+
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
 function onPlay({ seconds }) {
 	localStorage.setItem('videoplayer-current-time', seconds)
 };
+
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
