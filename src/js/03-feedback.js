4e1772680415406316749fcb9ac1eb3a46b4f614
SergiiMal <ser4ester@gmail.com>
2023-01-30 20:05:25 +0200

    3++

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,50 +1,28 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const STORAGE_KEY = 'feedback-form-state';
-
-
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
-
-dataLocalStorage();
 
 const formData = {};
 
 function onFormData(evt) {
-	formData[evt.target.name] = evt.target.value;
-	localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  formData[e.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function onSubmitForm(evt) {
-	console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-	evt.preventDefault();
-
-	formData.email = form.email.value;
-	formData.message = form.message.value;
-
-	if (formData.email === '' || formData.message === '') {
-		return;
-	}
-// 	console.log(formData);
-
-	evt.currentTarget.reset();
-	localStorage.removeItem(STORAGE_KEY);
-
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
 }
 
-
-function dataLocalStorage() {
-
-	// const email = document.querySelector('.feedback-form input');
-	// const message = document.querySelector('.feedback-form textarea')
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+	const message = document.querySelector('.feedback-form textarea');
 	
-	
-
-	if (localStorage.getItem(STORAGE_KEY)) {
-		const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-		form.email.value = data.email;
-		form.message.value = data.message;
-	}
-};
+  email.value = dataStoreage.email || '';
+  message.value = dataStoreage.message || '';
+})();
