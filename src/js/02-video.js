8723710db2b31e9a00eee853064442807782a8ce
SergiiMal <ser4ester@gmail.com>
2023-01-30 17:08:36 +0200

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,27 +2,19 @@
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time'; 
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-	localStorage.setItem('videoplayer-current-time', seconds)
+	localStorage.setItem(LOCALSTORAGE_KEY, seconds)
 };
 
 
 function load() {
-	const dataStoreage = localStorage.getItem('videoplayer-current-time')
+	const dataStoreage = localStorage.getItem(LOCALSTORAGE_KEY)
 	if (dataStoreage) {
 		player.setCurrentTime(dataStoreage);
 	}
-}
-// const load = key => {
-  
-//   try {
-//     const serializedState = localStorage.getItem(key);
-//     return serializedState === null ? undefined : JSON.parse(serializedState);
-//   } catch (error) {
-//     console.error("Get state error: ", error.message);
-//   }
-// };
+};
 
