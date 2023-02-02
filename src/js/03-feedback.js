be2f8552b91f3e4dbe8195589480f29da330454c
SergiiMal <ser4ester@gmail.com>
2023-02-02 17:32:24 +0200

    (:

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,19 +2,19 @@ import throttle from 'lodash.throttle';
 import { saveToStorage, loadFromStorage } from './storage.js';
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
+const recoverData = loadFromStorage(STORAGE_KEY);
 
 const dataInput = {
 	email: '',
 	message: '',
 };
 
+condition();
 function saveToInput(evt) {
 	dataInput.email = evt.currentTarget[0].value;
 	dataInput.message = evt.currentTarget[1].value;
 	 saveToStorage(STORAGE_KEY, dataInput);
 }
-
-
 
 function onSubmitForm(evt) {
 	evt.preventDefault();	
@@ -38,14 +38,14 @@ function storageCleaner() {
 
 form.addEventListener('input', throttle(saveToInput), 500);
 form.addEventListener('submit', onSubmitForm);
-const recoverData = loadFromStorage(STORAGE_KEY);
+
 
 function condition(){
-	if (loadFromStorage === undefined) {
-		 saveToStorage(STORAGE_KEY, dataInput);
+	if (recoverData=== undefined) {
+		return saveToStorage(STORAGE_KEY, dataInput);
 	}else {
 		 form.email.value = recoverData.email;
  	    form.message.value = recoverData.message;
 		} 
 };
-condition();
+
